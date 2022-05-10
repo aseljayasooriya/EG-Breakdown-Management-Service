2505cd72ec599183fa5b53ca78893ee188ad9083
aseljayasooriya <asel.jaya@gmail.com>
2022-05-10 18:47:49 +0530

    Project initial files and directories added

---
+++ BreakdownInformationService/src/main/java/com/BreakDownsAPI.java
@@ -0,0 +1,40 @@
+package com;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class BreakDownsAPI
+ */
+@WebServlet("/BreakDownsAPI")
+public class BreakDownsAPI extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public BreakDownsAPI() {
+        super();
+        
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		doGet(request, response);
+	}
+
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
